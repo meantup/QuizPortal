d3c418d08c399524b3b87bf32d8174c4e9b18857
Mocariza <mcocariza@gmail.com>
2021-12-02 16:32:25 +0800

    Add project files.

---
+++ QuizPortal/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizPortal.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult AccountLogin()
+        {
+            return View();
+        }
+        public async Task<ActionResult> Index(Account account)
+        {
+            return View();
+        }
+    }
+}
---
+++ QuizPortal/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizPortal.Models
+{
+    public class Account
+    {
+        public string studentName { get; set; }
+        public string section { get; set; }
+    }
+}
