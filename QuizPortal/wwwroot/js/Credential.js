b7546b41992c53ae1a38054d5682edf7fc762ed6
Mocariza <mcocariza@gmail.com>
2021-12-02 17:02:34 +0800

    UpdateChanges

---
+++ QuizPortal/wwwroot/js/Credential.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+    var formData = $('#formID').serialize();
+    var formHandler = document.getElementById('formHandler');
+    var path = formHandler.attr('action');
+    $.ajax({
+        type: 'POST',
+        url: path,
+        data: formData,
+        success: function (response, textStatus, xhr) {
+            if (xhr.status === 201) {
+                if (response.status === 30) {
+                    Swal.fire({
+                        text: response.message,
+                        title: response.title,
+                        icon: response.type,
+                        allowOutsideClick: false
+                    });
+                }
+            } else {
+
+            }
+        }, error: function (response) {
+            console.log(response);
+        }
+    });
+});
